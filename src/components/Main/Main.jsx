108191a2c6d5235bb4c771798bf221fa432f128e
Vitkor <vitland@gmail.com>
2023-07-29 14:14:44 +0300

    feat: Сверстать AboutProject component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,11 @@
 import Promo from '../Promo/Promo';
 import styles from './Main.module.css'
+import AboutProject from '../AboutProject/AboutProject';
 const Main = () => {
   return (
     <main className={styles.main}>
       <Promo/>
+      <AboutProject/>
     </main>
 
   );
